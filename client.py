c415cbd3be9b7cbaba197e9bdfda15dc950338c2
jenrinaldo <jenrinaldo@student.ub.ac.id>
2019-02-26 16:30:08 +0700

    Error Handling

--- client.py
+++ client.py
@@ -4,8 +4,11 @@ client = xmlrpc.client.ServerProxy("http://127.0.0.1:8888")
 
 daftar = client.beli(1000)
 
-if type(daftar) is list:
-    for hasil in daftar:
-        print(hasil)
-else:
-    print(daftar)
+try:
+    if type(daftar) is list:
+        for hasil in daftar:
+            print(hasil)
+    else:
+        print(daftar)
+except ValueError:
+    print("Error...")
